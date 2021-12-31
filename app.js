da87c8bc8d41563a5e7a4f5bf056849ff435ed03
audace-kwizera <companyfreshbeauty@gmail.com>
2021-12-31 20:43:09 +0100

    normalement tout marche a merveille

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-document.getElementById('trajet').addEventListener('submit', kilometreParcouru);
+document.getElementById('prixDuTrajet').addEventListener('submit', prixDuTrajet);
 
-function kilometreParcouru(e) {
+function prixDuTrajet(e) {
+
+    const userConso = document.getElementById("conso").value;
+    const userTrajet = document.getElementById("distanceParcouru").value;
+    const userPrix = document.getElementById("prixCarburant").value;
+
+    console.log(userConso, userTrajet, userPrix);
+
+    //Resultat
+    const userTarifConso = parseFloat(userConso) / 100;
+    const userTarifTrajet = parseFloat(userTrajet);
+    const userTarifCarburant = parseFloat(userPrix);
+
+    const userPrixTrajet = (userTarifConso * userTarifTrajet * userTarifCarburant).toFixed(2);
+
+    document.getElementById("tarifConso").innerHTML = "$" + userPrixTrajet;
     e.preventDefault();
 }
